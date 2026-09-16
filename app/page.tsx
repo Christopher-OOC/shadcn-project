6b4b16ad6e895d71f0a7566f1ab06ec5f7c7fda4
Christopher-OOC <olojedechristopher24@gmail.com>
2026-09-16 06:49:22 +0100

    commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import CustomButton from "@/components/CustomButton";
+import { Button } from "@/components/ui/button";
+import { CirclePlus } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+     
+    </div>
+  );
+}
---
+++ components/AppSidebar.tsx
@@ -0,0 +1,72 @@
+import { CalendarHeart, Home, Inbox, Search, Settings } from "lucide-react";
+import React from "react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calender",
+    url: "/",
+    icon: CalendarHeart,
+  },
+  {
+    title: "Search",
+    url: "/",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "/",
+    icon: Settings,
+  },
+];
+
+const AppSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton>
+                    <Link href={item.url}>
+                      <item.icon />
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
